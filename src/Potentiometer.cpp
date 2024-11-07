0ea18924948b45a55914522c22cfe83a57feefc4
Skeitt <85678814+Skeitt@users.noreply.github.com>
2024-11-07 14:54:48 +0100

    added delay
    added delay after sending a message, removed debug flags

--- src/Potentiometer.cpp
+++ src/Potentiometer.cpp
@@ -5,20 +5,23 @@ Potentiometer::Potentiometer() : m_value(0), m_rpm(0), m_torque_pos(0), m_torque
 
 void Potentiometer::update()
 {
-    m_value = analogRead(POTENTIOMETER_PIN);
+    m_rpm = 100;
+    m_torque_pos = 100;
+    m_torque_neg = 0;
+    // m_value = analogRead(POTENTIOMETER_PIN);
 
-    if (m_value > 180)
-    {
-        m_rpm = map(m_value, 115, 970, 0, 6000);
-        m_torque_pos = 100;
-        m_torque_neg = 0;
-    }
-    else
-    {
-        m_rpm = 0;
-        m_torque_pos = 0;
-        m_torque_neg = 0;
-    }
+    // if (m_value > 180)
+    // {
+    //     m_rpm = map(m_value, 115, 970, 0, 6000);
+    //     m_torque_pos = 100;
+    //     m_torque_neg = 0;
+    // }
+    // else
+    // {
+    //     m_rpm = 0;
+    //     m_torque_pos = 0;
+    //     m_torque_neg = 0;
+    // }
 }
 
 int16_t Potentiometer::getRpm() const { return m_rpm; }
